122c0fe95b617de1daa6e114512658cf58b27a21
Ares DoDox <luyn14@gmail.com>
2019-05-16 20:04:28 +0700

    create Store Redux

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to React with Redux project</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
